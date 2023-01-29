52e7cf1795a274448c4c50564da60d2fc20b0749
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-29 13:45:49 +0200

    start the solution

--- cpp/atbash-cipher/atbash_cipher.h
+++ cpp/atbash-cipher/atbash_cipher.h
@@ -1,8 +1,11 @@
 #if !defined(ATBASH_CIPHER_H)
 #define ATBASH_CIPHER_H
 
-namespace atbash_cipher {
+#include <string>
 
+namespace atbash_cipher {
+    std::string encode(const std::string& text);
+    std::string decode(const std::string& text);
 }  // namespace atbash_cipher
 
 #endif // ATBASH_CIPHER_H
