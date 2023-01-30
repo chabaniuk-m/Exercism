ea2f20e6d56c93c25b792591dad9a2a52f9b0e01
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-30 16:50:59 +0200

    bank account solved

--- cpp/bank-account/bank_account.cpp
+++ cpp/bank-account/bank_account.cpp
@@ -1,3 +1,40 @@
 #include "bank_account.h"
 
-namespace Bankaccount {}
+#include <stdexcept>
+
+namespace Bankaccount {
+
+    Bankaccount::Bankaccount(): is_open(false), _balance(0){}
+
+    int Bankaccount::balance() const {
+        if (!is_open) throw std::runtime_error("Account is closed");
+        return this->_balance;
+    }
+
+    void Bankaccount::deposit(int amount) {
+        if (!is_open || amount < 0) throw std::runtime_error("Account is closed");
+        m.lock();
+        this->_balance += amount;
+        m.unlock();
+    }
+
+    int Bankaccount::withdraw(int amount) {
+        if (!is_open || amount < 0) throw std::runtime_error("Account is closed");
+        if (amount > this->_balance) throw std::runtime_error("Insufficient number of funds on the balance");
+        m.lock();
+        this->_balance -= amount;
+        m.unlock();
+        return amount;
+    }
+
+    void Bankaccount::close() {
+        if (!is_open) throw std::runtime_error("Account is already closed");
+        this->is_open = false;
+    }
+
+    void Bankaccount::open() {
+        if (is_open) throw std::runtime_error("Account is already open");
+        this->is_open = true;
+        this->_balance = 0;
+    }
+}
--- cpp/bank-account/bank_account.h
+++ cpp/bank-account/bank_account.h
@@ -1,9 +1,22 @@
 #if !defined(BANK_ACCOUNT_H)
 #define BANK_ACCOUNT_H
 
-namespace Bankaccount {
-class Bankaccount {};  // class Bankaccount
+#include <mutex>
 
+namespace Bankaccount {
+    class Bankaccount {
+    private:
+        bool is_open;
+        int _balance;
+        std::mutex m;
+    public:
+        Bankaccount();
+        [[nodiscard]] int balance() const;
+        void deposit(int amount);
+        int withdraw(int amount);
+        void open();
+        void close();
+    };  // class Bankaccount
 }  // namespace Bankaccount
 
 #endif  // BANK_ACCOUNT_H
