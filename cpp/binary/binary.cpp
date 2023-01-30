d310492bf5eb8322aad9fed399ee7db6ada6e649
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-30 19:25:30 +0200

    binary solved

--- cpp/binary/binary.cpp
+++ cpp/binary/binary.cpp
@@ -1,5 +1,14 @@
 #include "binary.h"
+#include <algorithm>
 
 namespace binary {
 
+    int convert(const std::string &binary) {
+        if (!std::all_of(binary.cbegin(), binary.cend(), isdigit)) return 0;
+        int decimal = 0;
+        for (int i = binary.length() - 1, p = 0; i >= 0; --i, ++p) {
+            decimal += (binary[i] - '0') * int(pow(2, p));
+        }
+        return decimal;
+    }
 }  // namespace binary
