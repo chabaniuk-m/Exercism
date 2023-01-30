75628bf512c4909457a13782ca10fe6d14037a76
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-30 16:25:16 +0200

    base convert solved

--- cpp/all-your-base/all_your_base.cpp
+++ cpp/all-your-base/all_your_base.cpp
@@ -1,5 +1,24 @@
 #include "all_your_base.h"
 
+#include <stdexcept>
+#include <cmath>
+
 namespace all_your_base {
 
+    std::vector<unsigned int> convert(int a, const std::vector<unsigned int> &number, int b) {
+        if (a < 2 || b < 2) throw std::invalid_argument("Each base must be 2 or more");
+        for (auto d : number) {
+            if (d >= a) throw std::invalid_argument("Number is invalid");
+        }
+        unsigned int decimal = 0;
+        for (int i = int(number.size()) - 1, p = 0; i >= 0; --i, ++p) {
+            decimal += number[i] * std::pow(a, p);
+        }
+        std::vector<unsigned int> result;
+        while (decimal > 0) {
+            result.insert(result.begin(), decimal % b);
+            decimal /= b;
+        }
+        return result;
+    }
 }  // namespace all_your_base
--- cpp/all-your-base/all_your_base.h
+++ cpp/all-your-base/all_your_base.h
@@ -1,8 +1,10 @@
 #if !defined(ALL_YOUR_BASE_H)
 #define ALL_YOUR_BASE_H
 
-namespace all_your_base {
+#include <vector>
 
+namespace all_your_base {
+    std::vector<unsigned int> convert(int a, const std::vector<unsigned int> &number, int b);
 }  // namespace all_your_base
 
 #endif // ALL_YOUR_BASE_H
