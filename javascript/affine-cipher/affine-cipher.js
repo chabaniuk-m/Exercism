9fc1f1ca085f922190777fd12586a470ba7e30b0
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-31 00:48:59 +0200

    affine cipher completed

--- javascript/affine-cipher/affine-cipher.js
+++ javascript/affine-cipher/affine-cipher.js
@@ -1,7 +1,75 @@
+let a_code = 'a'.charCodeAt(0)
+
+function gcd(a, b) {
+  return !b ? a : gcd(b, a % b)
+}
+
+function check(a) {
+  if (gcd(a, 26) !== 1)
+    throw new Error('a and m must be coprime.')
+}
+
+function isAlpha(c) {
+  let z = 'z'.charCodeAt(0)
+  return a_code <= c.charCodeAt(0) && c.charCodeAt(0) <= z;
+}
+
+function isDigit(c) {
+  let zero = '0'.charCodeAt(0)
+  let nine = '9'.charCodeAt(0)
+  return zero <= c.charCodeAt(0) && c.charCodeAt(0) <= nine;
+}
+
 export const encode = (phrase, key) => {
-  throw new Error('Remove this statement and implement this function');
+  phrase = phrase.toLowerCase()
+  const { a, b } = key
+  check(a)
+  let encoded = ""
+  const addSymbol = (code) => {
+    encoded += String.fromCharCode([code])
+  }
+  for (let i = 0, k = 0; i < phrase.length; i++) {
+    if (isAlpha(phrase.charAt(i))) {
+      addSymbol((a * (phrase.charCodeAt(i) - a_code) + b) % 26 + a_code)
+      k += 1
+      if (k === 5) {
+        encoded += " "
+        k = 0
+      }
+    } else if (isDigit(phrase.charAt(i))) {
+      encoded += phrase.charAt(i)
+      k += 1
+      if (k === 5) {
+        encoded += " "
+        k = 0
+      }
+    }
+  }
+  if (encoded[encoded.length - 1] === ' ')
+    return encoded.substring(0, encoded.length - 1)
+  return encoded;
 };
 
+function mmi(a) {
+  for (let i = 1; i < 26; i++) {
+    if (a * i % 26 === 1) return i
+  }
+}
+
 export const decode = (phrase, key) => {
-  throw new Error('Remove this statement and implement this function');
+  phrase = phrase.toLowerCase()
+  let { a, b } = key
+  check(a)
+  let decoded = ""
+  a = mmi(a)
+  for (const c of phrase) {
+    if (isAlpha(c)) {
+      let code = c.charCodeAt(0) - a_code - b
+      while (code < 0) code += 26
+      decoded += String.fromCharCode([a * code % 26 + a_code])
+    }
+    else if (isDigit(c))
+      decoded += c
+  }
+  return decoded
 };
