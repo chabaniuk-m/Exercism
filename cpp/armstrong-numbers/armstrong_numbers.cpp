6b3e7091e22c6ae13935f930bfb7400044066e15
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-30 16:30:55 +0200

    armstrong number solved

--- cpp/armstrong-numbers/armstrong_numbers.cpp
+++ cpp/armstrong-numbers/armstrong_numbers.cpp
@@ -1,5 +1,16 @@
 #include "armstrong_numbers.h"
+#include <cmath>
 
 namespace armstrong_numbers {
 
+    bool is_armstrong_number(int n) {
+        int num = n;
+        int power = int(std::log10(n)) + 1;
+        int sum = 0;
+        while (n > 0) {
+            sum += int(std::pow(n % 10, power));
+            n /= 10;
+        }
+        return sum == num;
+    }
 }  // namespace armstrong_numbers
