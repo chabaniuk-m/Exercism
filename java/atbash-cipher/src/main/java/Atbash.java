441361a4904368d84c7e258f8ec1bfacaec6345a
chabanyukm <chabanyuk.maks@gmail.com>
2023-02-05 22:51:11 +0200

    solved

--- java/atbash-cipher/src/main/java/Atbash.java
+++ java/atbash-cipher/src/main/java/Atbash.java
@@ -9,11 +9,27 @@ Please remove this comment when submitting your solution.
 
 */
 public class Atbash {
-    public String encode(String text) {
+    private static final char a = 'a';
 
+    public String encode(String text) {
+        StringBuilder encoded = new StringBuilder(text.length() * 3 / 2);
+        int k = 0;
+        for (char c : text.toLowerCase().toCharArray())
+            if (Character.isAlphabetic(c)) {
+                k += 1;
+                encoded.append((char) (25 - (c - a) + 'a'));
+                if (k % 5 == 0)
+                    encoded.append(' ');
+            } else if (Character.isDigit(c)) {
+                k += 1;
+                encoded.append(c);
+                if (k % 5 == 0)
+                    encoded.append(' ');
+            }
+        return encoded.charAt(encoded.length() - 1) == ' ' ? encoded.substring(0, encoded.length() - 1) : encoded.toString();
     }
 
     public String decode(String text) {
-        
+        return encode(text).replaceAll("\\s", "");
     }
 }
