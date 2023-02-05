5bbbd1e46b40b3792a4539ce7b3cddee6182a8a4
chabanyukm <chabanyuk.maks@gmail.com>
2023-02-05 23:22:49 +0200

    solved

--- java/binary-search/src/main/java/BinarySearch.java
+++ java/binary-search/src/main/java/BinarySearch.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /*
 
 Since this exercise has a difficulty of > 4 it doesn't come
@@ -8,3 +10,27 @@ which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 
 */
+public class BinarySearch {
+    private final List<Integer> list;
+
+    public BinarySearch(List<Integer> list) {
+        this.list = list;
+    }
+
+    public int indexOf(int element) throws ValueNotFoundException {
+        int begin = 0;
+        int end = list.size() - 1;
+        if (end == -1)
+            throw new ValueNotFoundException("Value not in array");
+        while (begin < end - 1) {
+            int mid = (begin + end) / 2;
+            int val = list.get(mid);
+            if (val == element) return  mid;
+            else if (val < element) begin = mid;
+            else end = mid;
+        }
+        if (list.get(end) == element) return end;
+        if (list.get(begin) == element) return begin;
+        throw new ValueNotFoundException("Value not in array");
+    }
+}
