c6b1411930a5737a51920536f0fc83e15ad0bb18
chabanyukm <chabanyuk.maks@gmail.com>
2023-02-05 22:32:35 +0200

    solved

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
 
 Since this exercise has a difficulty of > 4 it doesn't come
@@ -8,3 +10,26 @@ which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 
 */
+public class Anagram {
+    private final String word;
+
+    public Anagram(String word) {
+        this.word = word;
+    }
+
+    public List<String> match(Collection<String> words) {
+        final var counts = charCount(word.toLowerCase());
+        return new ArrayList<>() {{
+            for (var w : words)
+                if (!Objects.equals(w.toLowerCase(), word.toLowerCase()) &&
+                    charCount(w.toLowerCase()).equals(counts)) add(w);
+        }};
+    }
+
+    private Map<Character, Integer> charCount(final String word) {
+        return new HashMap<>(){{
+            for (char c : word.toCharArray())
+                put(c, containsKey(c) ? get(c) + 1 : 1);
+        }};
+    }
+}
