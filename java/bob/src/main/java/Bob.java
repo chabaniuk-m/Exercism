a7009efcb910bcf65d62665e4bceafa7f9ba4a1c
chabanyukm <chabanyuk.maks@gmail.com>
2023-02-06 00:02:42 +0200

    solved

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -8,3 +8,29 @@ which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 
 */
+public class Bob {
+    private static boolean isSpace(String s) {
+        for (char c : s.toCharArray())
+            if (!Character.isSpaceChar(c))
+                return false;
+        return true;
+    }
+
+    private static boolean isUpper(String s) {
+        boolean atLeastOneLetter = false;
+        for (char c : s.toCharArray())
+            if (Character.isAlphabetic(c)) {
+                atLeastOneLetter = true;
+                if (!Character.isUpperCase(c))
+                    return false;
+            }
+        return atLeastOneLetter;
+    }
+
+    public String hey(String text) {
+        text = text.trim();
+        if (isSpace(text)) return "Fine. Be that way!";
+        if (isUpper(text)) return text.charAt(text.length() - 1) == '?' ? "Calm down, I know what I'm doing!" : "Whoa, chill out!";
+        return text.charAt(text.length() - 1) == '?' ? "Sure." : "Whatever.";
+    }
+}
